bdf4cd6ceebaa69a19d2ab38346a42437c0f5332
Galeev LXIX <gtimur477@gmail.com>
2024-07-20 02:27:54 +0300

    Добавьте файлы проекта.

---
+++ reginapractice2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace reginapractice2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Номер заказа";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(133, 21);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(240, 26);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 53);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1102, 615);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1102, 668);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ reginapractice2/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace reginapractice2
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection = null;
+
+        private List<string[]> data;
+        private List<string[]> data1;
+        int ord_num;
+        string t = null;
+        string t1 = null;
+        string t2 = null;
+        SqlDataReader reader;
+        SqlDataReader reader1;
+
+        public Form1()
+        {
+            data = new List<string[]>();
+            data1 = new List<string[]>();
+            ord_num = 1;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["myservicedb"].ConnectionString);
+            sqlConnection.Open();
+
+            string query1 = "select distinct orders.order_id, emp.first_name + ' ' + emp.last_name, emp.phone_number as employee_number, cl.first_name + ' ' + cl.last_name, cl.phone_number, orders.description, orders.status, orders.order_date, sum(serv.cost) over(partition by orders.order_id) from employees emp join services_rendered serv_rend on (emp.employee_id = serv_rend.employee_id) join services serv on (serv_rend.service_id = serv.service_id) join orders on (serv_rend.order_id = orders.order_id) join clients cl on (cl.client_id = orders.client_id);";
+            string query2 = "select orders.order_id, serv.service_name, serv.cost from services_rendered serv_rend join services serv on (serv_rend.service_id = serv.service_id) join orders on (serv_rend.order_id = orders.order_id) join clients cl  on (cl.client_id = orders.client_id) order by orders.order_id;";
+
+            SqlCommand command1 = new SqlCommand(query1, sqlConnection);
+            SqlCommand command2 = new SqlCommand(query2, sqlConnection);
+
+            reader = command1.ExecuteReader();
+            while (reader.Read())
+            {
+                data.Add(new string[9]);
+
+                data[data.Count - 1][0] = reader[0].ToString().Trim();
+                data[data.Count - 1][1] = reader[1].ToString().Trim();
+                data[data.Count - 1][2] = reader[2].ToString().Trim();
+                data[data.Count - 1][3] = reader[3].ToString().Trim();
+                data[data.Count - 1][4] = reader[4].ToString().Trim();
+                data[data.Count - 1][5] = reader[5].ToString().Trim();
+                data[data.Count - 1][6] = reader[6].ToString().Trim();
+                data[data.Count - 1][7] = reader[7].ToString().Trim();
+                data[data.Count - 1][8] = reader[8].ToString().Trim();
+            }
+
+            reader.Close();
+
+            reader1 = command2.ExecuteReader();
+            while (reader1.Read())
+            {
+                data1.Add(new string[3]);
+
+                data1[data1.Count - 1][0] = reader1[0].ToString().Trim();
+                data1[data1.Count - 1][1] = reader1[1].ToString().Trim();
+                data1[data1.Count - 1][2] = reader1[2].ToString().Trim();
+            }
+            reader1.Close();
+
+            sqlConnection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            for (int i = 0; i < data.Count(); i++)
+            {
+                if (Int32.Parse(data[i][0]) == ord_num)
+                {
+                    t = "Номер заказа: " + data[i][0];
+                    listView1.Items.Add(t);
+                    t = "Клиент: " + data[i][3] + "    |   Номер телефона клиента - " + data[i][4];
+                    listView1.Items.Add(t);
+                    t = "Мастер: " + data[i][1] + "    |   Номер телефона мастера - " + data[i][2];
+                    listView1.Items.Add(t);
+                    t = "Проблема: " + data[i][5];
+                    listView1.Items.Add(t);
+                    t = "Статус: " + data[i][6];
+                    listView1.Items.Add(t);
+                    t = "Дата заказа: " + data[i][7];
+                    listView1.Items.Add(t);
+                    t1 = "Услуги: ";
+                    for (int j = 0; j < data1.Count(); j++)
+                    {
+                        if (Int32.Parse(data[i][0]) == Int32.Parse(data1[j][0]))
+                        {
+                            t1 += data1[j][1] + " (" + data1[j][2] + "р)    ";
+                        }
+                    }
+                    listView1.Items.Add(t1.Trim());
+                    t = "Сумма к оплате: " + data[i][8];
+                    listView1.Items.Add(t);
+                }
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            ord_num = Int32.Parse(numericUpDown1.Value.ToString());
+        }
+    }
+}
